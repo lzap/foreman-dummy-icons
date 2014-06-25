708fbcfbd711287b64f987e28b575da84a8e21d9
Lukas Zapletal <lzap+git@redhat.com>
2014-06-25 16:55:44 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 foreman-dummy-icons
 ===================
 
-Dummy icons for Foreman project
+Dummy icons for The Foreman project. There is a generator to generate more.
+Usage:
+
+    ./generate.sh color size
+
+For instance:
+
+    ./generate.sh red 21
+
+To find supported colors head over to http://www.graphicsmagick.org/color.html
+
+Authors
+=======
+
+* ppisar_at_redhat_dot_com
+* lzap_at_redhat_dot_com
+
+License
+=======
+
+GNU GPL v2+
---
+++ generate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+COLOR=${1:-black}
+SIZE=${2:-21}
+LABELS="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+
+mkdir -p "${SIZE}x${SIZE}-$COLOR" 2>/dev/null
+
+for L in $LABELS; do
+  gm convert \
+    -size ${SIZE}x${SIZE} "xc:$COLOR" \
+    -fill white \
+    -pointsize 21 \
+    -gravity center \
+    -draw "text 0,2 '$L'" \
+    -fill none \
+    -stroke white \
+    -draw "rectangle 0,0,20,20" \
+    "${SIZE}x${SIZE}-$COLOR/${L,,}.png"
+done
