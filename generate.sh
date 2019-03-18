cf5ecbc48f614dae27d7aa4c1b28cc22764cdcad
Lukas Zapletal <lzap+git@redhat.com>
2019-03-18 14:26:15 +0100

    Dropped 21x21

--- generate.sh
+++ generate.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-COLOR=${1:-black}
-SIZE=${2:-21}
-LABELS="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+SIZE=${1:-16}
+LABELS="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z #"
+COLORS="black blue darkblue darkred darkslateblue deepskyblue firebrick forestgreen goldenrod green red steelblue"
 
-mkdir -p "${SIZE}x${SIZE}-$COLOR" 2>/dev/null
+mkdir -p "${SIZE}x${SIZE}" 2>/dev/null
 
-for L in $LABELS; do
-  gm convert \
-    -size ${SIZE}x${SIZE} "xc:white" \
-    -fill $COLOR \
-    -stroke $COLOR \
-    -draw "circle $(($SIZE/2)),$(($SIZE/2)),$(($SIZE/2)),0" \
-    -fill white \
-    -stroke none \
-    -pointsize $(($SIZE-4)) \
-    -gravity center \
-    -draw "text 1,1 '$L'" \
-    "${SIZE}x${SIZE}-$COLOR/${L,,}.png"
+for COLOR in $COLORS; do
+  for L in $LABELS; do
+    gm convert \
+      -size ${SIZE}x${SIZE} "xc:white" \
+      -fill $COLOR \
+      -stroke $COLOR \
+      -draw "circle $(($SIZE/2)),$(($SIZE/2)),$(($SIZE/2)),0" \
+      -fill white \
+      -stroke none \
+      -pointsize $(($SIZE-4)) \
+      -gravity center \
+      -font /usr/share/fonts/gnu-free/FreeMonoBold.ttf \
+      -draw "text 1,1 '$L'" \
+      /tmp/foreman-dummy.png
+    pngcrush /tmp/foreman-dummy.png "${SIZE}x${SIZE}/$COLOR-${L,,}.png"
+  done
 done
